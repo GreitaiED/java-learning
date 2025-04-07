76da0f05e96faf80e9b088a22e15d8e5ee7c55d7
edvards_Malina <edvard1407@gmail.com>
2025-04-07 20:38:07 +0300

    [feature/lesson_1]: Learn Java Variables

--- src/main/java/lvacademy/MyClass.java
+++ src/main/java/lvacademy/MyClass.java
@@ -2,51 +2,40 @@ package lvacademy;
 
 public class MyClass {
     public static void main(String[] args) {
-        // sout -> shortcut for System.out.println("");
-        System.out.println("Ja u Mami programmist!");
-
-        // Your text is here (one-line comment)
-
+        // sout
+        System.out.println("pelemeni");
+        // text
         /*
-        First line
-        Second line
-        Third line
+        text
          */
-
-        // Primitive data types:
-
-        // byte 8-bit
-        byte age = 30;
+        byte age = 52;
         System.out.println(age);
 
-        // Naming convention:
-        // Camel Case: MyClass for classes (Capital Camel Case)
-        // Camel Case: currentYear for variables (Lower Camel Case)
-
+        // Maning covention;
+        // Conel Case: MyClass for yout classes (Capital Canel Case)
+        // Canel case: corrent for variable (lower Canel Case)
         // short 16-bit
         short currentYear = 2025;
-        short temperature = -273;
-        short distance = 1600;
+        short teamperatur = -1000;
 
         // int 32-bit
         int score = 9000;
-        int population = 144400000;
-        int result = (10 * 10) - 42;
-        System.out.println(result);
+        int pelemeska = 1000000000;
+        int result = (10+10) - 52;
 
-        // long 64-bit
-        long starsInGalaxy = 100_000_000_000L; // 100000000000
-        long bigNumber = 929287266127172162L;
-        long currentMillis = System.currentTimeMillis();
-        System.out.println(currentMillis);
+        // long 62-bit
+        long starsInGalaxy = 100_000_000L; // 1000000000
+        long HowMuchPelemeniIEaten = 3123231321L;
+        long currentTimeMillis = System.currentTimeMillis();
+        System.out.println(currentTimeMillis);
 
         // float 32-bit
-        float pi = 3.14f;
+        float pi = 3.14159265358979f;
         float piNumber = (float) Math.PI; // casting
-        float discount = 25.5f;
+        float discount = 25.1f;
 
-        // double 64-bit
-        double accountBalance = 99999.99999944444;
+        // double 62-bit
+        double accountBalance = 10000.00;
         double squareRoot = Math.sqrt(2);
         System.out.println(squareRoot);
         double piDouble = Math.PI;
@@ -55,21 +44,21 @@ public class MyClass {
         System.out.println(piDouble);
 
         // char (symbol)
-        char initial = 'N';
+        char initila = 'N';
         char digit = '7';
         char symbol = '#';
 
-        // See unicode table https://symbl.cc/en/unicode-table/
-        char smiley = '\u263A';
+        // See unicode table https://sumbl.cc/en/unicode-table/
+        char smiley = '\u263a';
         System.out.println(smiley);
 
         // boolean (true/false)
         boolean isJavaFun = true;
         boolean isTired = true;
-        boolean passed = (40 > 60); // false
+        boolean passed = (40 >60); // false
 
-        // String
-        String myFullName = "Nikita Milka"; // immutable
-        String mfn = new String("Nikita Milka");
+        //String
+        String myFullName = "Edvards"; // immutable
+        String mfn = new String("Edvards");;
     }
 }
